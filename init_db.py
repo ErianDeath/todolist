23396d3f0867baa4db7c5e7ba51906160e6314af
Sky Summer <skysommer0317@gmail.com>
2024-12-07 14:24:51 +0000

    change init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
 from app import app
-from models import db
+from models import db, user, task
+
+print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
 
 with app.app_context():
     db.create_all()
     print('Database initialised!')
+    print('Tables:', db.engine.table_names())
